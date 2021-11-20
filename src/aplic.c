777c6d07f6ae8083e5f95ee83db88d56b7976e3a
Diogo Costa <64315652+diogolhc@users.noreply.github.com>
2021-11-20 21:19:18 +0000

    Implemented receive_file, needs testing after data-link implemented

--- src/aplic.c
+++ src/aplic.c
@@ -10,7 +10,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define CONTROL_PACKET_MAX_SIZE 500
+#define CONTROL_PACKET_MAX_SIZE 500 // TODO check if good
+#define PACKET_MAX_SIZE (CONTROL_PACKET_MAX_SIZE > DATA_PACKET_MAX_SIZE ? CONTROL_PACKET_MAX_SIZE : DATA_PACKET_MAX_SIZE)
+#define FILE_NAME_MAX_SIZE 255
 
 #define C_DATA 0x1
 #define C_START 0x2
@@ -20,11 +22,10 @@
 
 #define L1(K) ((K) & 0b11111111)
 #define L2(K) (((K) >> 8) & 0b11111111)
+#define K(L1,L2) (256*(L2)+(L1))
 
 #define T_FILE_SIZE 0x0
 #define T_FILE_NAME 0x1
-
-#define L(v) (v)
 
 // TODO check if the return with errors should return also or enforce here other attempts
 
@@ -38,8 +39,7 @@ static off_t get_file_size(int fd) {
     return s.st_size;
 }
 
-// file_name must be a small string (like <100 bytes) define later
-static unsigned char* get_control_packet(off_t file_size, char *file_name, int *length) {
+static unsigned char* get_control_packet(off_t file_size, char *file_name, int file_name_size, int *length) {
     unsigned char* control_packet = malloc(CONTROL_PACKET_MAX_SIZE);
     if (control_packet == NULL) {
         return NULL;
@@ -56,7 +56,6 @@ static unsigned char* get_control_packet(off_t file_size, char *file_name, int *
 
     control_packet[i++] = T_FILE_NAME; // T2
 
-    size_t file_name_size = strlen(file_name);
     control_packet[i++] = (unsigned char)file_name_size; // L2
 
     memcpy(&control_packet[i], file_name, file_name_size); // V2
@@ -77,7 +76,7 @@ static int send_packaged_file(int fd_serial_port, int fd_file) {
     while (1) {
         data_packet[1] = sequence_number;
         sequence_number = (sequence_number+1) % 255;
-        
+
         ssize_t num = read(fd_file, &data_packet[4], DATA_PACKET_MAX_SIZE-4);
 
         if (num == -1) {
@@ -100,8 +99,13 @@ static int send_packaged_file(int fd_serial_port, int fd_file) {
     return 0;
 }
 
-// name_to_give must be a small string (like <100 bytes) define later
-int send_file(int porta, char *path, int path_size, char *name_to_give) {
+int send_file(int porta, char *path, int path_size, char *file_name) {
+    int file_name_size = strlen(file_name);
+    if (file_name_size > FILE_NAME_MAX_SIZE) {
+        fprintf(stderr, "File name to big.\n");
+        return -1;
+    }
+
     int fd_file;
     if ((fd_file = open(path, O_RDONLY)) < 0) {
         return -1;
@@ -121,7 +125,7 @@ int send_file(int porta, char *path, int path_size, char *name_to_give) {
 
     unsigned char* control_packet = NULL;
     int control_packet_size = 0;
-    if ((control_packet = get_control_packet(file_size, name_to_give, &control_packet_size)) == NULL) {
+    if ((control_packet = get_control_packet(file_size, file_name, file_name_size, &control_packet_size)) == NULL) {
         close(fd_file);
         llclose(fd_serial_port);
         return -1;
@@ -157,16 +161,104 @@ int send_file(int porta, char *path, int path_size, char *name_to_give) {
     return 0;
 }
 
-
 int receive_file(int porta) {
-    int fd;
-    if ((fd = llopen(porta, RECEIVER)) < 0) {
+    int fd_serial_port;
+    if ((fd_serial_port = llopen(porta, RECEIVER)) < 0) {
         return -1;
     }
 
-    // TODO
+    unsigned char *packet = malloc(PACKET_MAX_SIZE);
+    if (packet == NULL) {
+        llclose(fd_serial_port);
+        return -1;
+    }
 
-    if (llclose(fd) < 0) {
+    int fd_file_to_write = -1;
+    int sequence_number = 0;
+    off_t file_size = 0;
+
+    int not_end_packet = TRUE;
+    while (not_end_packet) {
+        int packet_size = 0;
+        if ((packet_size = llread(fd_serial_port, packet)) < 0) {
+            free(packet);
+            llclose(fd_serial_port);
+            return -1;
+        }
+
+        unsigned char control_field = packet[0];
+        switch (control_field) {
+        case C_DATA:
+            if (fd_file_to_write == -1) { // Control start packet didn't arrive yet
+                break;
+            }
+
+            int N = packet[1]; // TODO what to do if N doesn't check out with previous+1? since it's not supposed to check for errors in application layer...
+
+            // if N == sequence_number before, ignores current packet,
+            // but what if there is like a sequence_number before + 2?
+            // no way here to ask for the missing packet...
+
+
+            int num_octets = K(packet[3], packet[2]);
+            if (write(fd_file_to_write, &packet[4], num_octets) == -1) {
+                free(packet);
+                llclose(fd_serial_port);
+                return -1;
+            }
+
+            break;
+        
+        case C_START:;
+            int i = 1;
+            while (i < packet_size) {
+                unsigned char T = packet[i++];
+                unsigned char L = packet[i++];
+
+                if (T == T_FILE_SIZE) {
+                    memcpy(&file_size, &packet[i], L);
+
+                } else if (T == T_FILE_NAME) {
+                    char file_name[FILE_NAME_MAX_SIZE];
+                    memcpy(file_name, &packet[i], L);
+
+                    fd_file_to_write = open(file_name, O_WRONLY | O_APPEND | O_CREAT, 0644);
+                    if (fd_file_to_write == -1) {
+                        free(packet);
+                        llclose(fd_serial_port);
+                        return -1;
+                    }
+
+                } else {
+                    // TODO error, should it do something?
+                }
+
+                i += L;
+            }
+
+            break;
+
+        case C_END:
+            // TODO should it check equality in all the fields?
+            // what to do if they are different?
+            // data-link layer should've handled that
+
+
+            if (fd_file_to_write != -1) { 
+                close(fd_file_to_write);
+                not_end_packet = FALSE;
+            }
+            // else Control start packet didn't arrive yet, so wait for it
+
+            break;
+        
+        default:
+            break; // invalid control field (ignore packet)
+        }
+    }
+    
+    if (llclose(fd_serial_port) < 0) {
+        free(packet);
         return -1;
     }
 
--- src/aplic.h
+++ src/aplic.h
@@ -1,3 +1,3 @@
-int send_file(int porta, char *path, int path_size, char *name_to_give);
+int send_file(int porta, char *path, int path_size, char *file_name);
 
 int receive_file(int porta);
