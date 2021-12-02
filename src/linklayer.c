c029ff88f8741c8622893bcd99635de494c37818
Diogo Costa <64315652+diogolhc@users.noreply.github.com>
2021-12-02 09:59:40 +0000

    macros to .c

--- src/linklayer.c
+++ src/linklayer.c
@@ -11,6 +11,32 @@
 #include <signal.h>
 #include <stdint.h>
 
+#define FLAG 0x7E
+#define ESC 0x7D
+#define A 0x03
+#define C_SET 0x03
+#define C_DISC 0x0B
+#define C_UA 0X07
+
+#define C_RR(r) (0x05 | (((r) << 7) & 0x80))
+#define C_REJ(r) (0x01 | (((r) << 7) & 0x80))
+#define C_I(s) ((s) << 6)
+
+#define CONTROL_SIZE 5
+
+#define STUFFER 0x20
+
+#define TIME_OUT_TIME 3
+#define MAX_NO_TIMEOUT 3
+
+typedef enum control_frame_type {
+    SET,
+    DISC,
+    UA,
+    RR,
+    REJ
+}  control_frame_type_t;
+
 typedef enum state_sv_frame {
     START,
     FLAG_RCV,
--- src/linklayer.h
+++ src/linklayer.h
@@ -5,34 +5,8 @@
 #define FALSE 0
 #define TRUE 1
 
-#define FLAG 0x7E
-#define ESC 0x7D
-#define A 0x03
-#define C_SET 0x03
-#define C_DISC 0x0B
-#define C_UA 0X07
-
-#define C_RR(r) (0x05 | (((r) << 7) & 0x80))
-#define C_REJ(r) (0x01 | (((r) << 7) & 0x80))
-#define C_I(s) ((s) << 6)
-
-#define CONTROL_SIZE 5
-
-#define STUFFER 0x20
-
-#define TIME_OUT_TIME 3
-#define MAX_NO_TIMEOUT 3
-
 #define FRAME_MAX_SIZE 1010
 #define HEADER_AND_TAIL_SIZE 10 // more than enough
-
-typedef enum control_frame_type {
-    SET,
-    DISC,
-    UA,
-    RR,
-    REJ
-}  control_frame_type_t;
 
 typedef enum type {
     TRANSMITTER,
